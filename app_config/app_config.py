92511ad40fbd3741622a169f0d96a70dedabaa37
brett55 <bjcull2@gmail.com>
2016-03-11 12:59:36 -0700

    Updated tests to reflect refactor

--- app_config/app_config.py
+++ app_config/app_config.py
@@ -7,19 +7,8 @@ This implementation finds it's resources in the DynamoDB table called 'config_pr
 import boto
 import boto.dynamodb
 import json
-import time
+from boto.exception import BotoClientError
 from collections import defaultdict
-
-
-def timing(f):
-    def wrap(*args):
-        time1 = time.time()
-        ret = f(*args)
-        time2 = time.time()
-        print '%s function took %0.3f ms' % (f.func_name, (time2 - time1) * 1000.0)
-        return ret
-
-    return wrap
 
 
 class ConfigProvider(object):
@@ -34,7 +23,6 @@ class ConfigProvider(object):
         self._conn = boto.dynamodb.connect_to_region(region_name=region)
         self._table = self._conn.get_table(table_name)
 
-    @timing
     def __getitem__(self, resource_name):
         if not self._resources[resource_name]:
             self._load_resources_from_dynamo_db(resource_name, self._ENVIRONMENT_DEFAULT_NAME)
@@ -48,11 +36,7 @@ class ConfigProvider(object):
             if item:
                 resource_dict = json.loads(item.get("config"))
                 self._resources[resource_name].update(resource_dict)
+        except BotoClientError as e:
+            print(e.message)
         except Exception as e:
-            #raise Exception('Problem decoding json in {0}. Reason: {1}'.format(resource_name, e.message))
-            pass
-
-
-c = ConfigProvider("us-east-1", "unit_test_env")
-print c["workflows"]["username"]
-print c["workflows"]["username"]
+            raise Exception('Problem decoding json in {0}. Reason: {1}'.format(resource_name, e.message))
